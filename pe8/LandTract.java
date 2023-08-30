2c325d7f850b52509be26bc494351e30a4acaac7
KevMn6ix <kevin.munto@efrei.net>
2023-08-30 17:06:57 -0400

    ajout pe8

---
+++ pe8/LandTract.java
@@ -0,0 +1,34 @@
+package pe8;
+
+public class LandTract {
+    private double length;
+    private double width;
+
+    public LandTract(){
+
+    }
+    public LandTract(double length, double width ){
+        this.length = length;
+        this.width = width;
+    }
+
+    // Getters
+    public double getLength(){return length;}
+    public double getWidth(){return width;}
+
+    // Setters
+    public void setLength(double l){ length = l;}
+    public void setWidth(double w){width = w;}
+
+    public double getArea(double length, double width){
+        return length*width;
+    }
+
+    public boolean equals(LandTract l2){
+        if(l2 == null){
+            return false;
+        } else{
+            return (length == l2.length && width == l2.width);
+        }
+    }
+}
---
+++ pe8/Main.java
@@ -0,0 +1,8 @@
+package pe8;
+
+public class Main {
+    public static void main(String[] args){
+        LandTract l1 = new LandTract(2, 2);
+        System.out.println(getWidth() +" " +);
+    }
+}
